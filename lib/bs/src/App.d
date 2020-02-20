a3e199523aaa1a1d904d3b15e1100c1772eeff92
Covrick <asdfsdev264@gmail.com>
2020-02-20 20:14:14 +0900

    Remove unnecessery files

--- lib/bs/src/App.d
+++
@@ -1,1 +0,0 @@
-src\App-SimpleChatAppPortfolio.cmj : SimpleChatAppPortfolio.cmi
--- lib/bs/src/index.d
+++
@@ -1,1 +0,0 @@
-src\index-SimpleChatAppPortfolio.cmj : SimpleChatAppPortfolio.cmi src\App-SimpleChatAppPortfolio.cmj src\App-SimpleChatAppPortfolio.cmi
